4705f1178f2007b1f77c6135c55e73a7ba5b7bbd
Nicolo' Chiarello <chiarello.nicolo1@gmail.com>
2024-11-24 21:01:22 +0100

    added handler to save session to local storage

---
+++ src/TodoTogether/TodoTogether/Security/LocalStorageSessionHandler.cs
@@ -0,0 +1,58 @@
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components;
+using Newtonsoft.Json;
+using Supabase.Gotrue;
+using Supabase.Gotrue.Interfaces;
+
+namespace TodoTogether.Security;
+
+public class LocalStorageSessionHandler : IGotrueSessionPersistence<Session>
+{
+    private readonly ISyncLocalStorageService _localStorage;
+    
+    private const string SessionKey = "todotogether.session";
+
+
+    public LocalStorageSessionHandler(ISyncLocalStorageService localStorage)
+    {
+        _localStorage = localStorage;
+    }
+    
+    public void SaveSession(Session session)
+    {
+        try
+        {
+            var serializedSession = JsonConvert.SerializeObject(session);
+            _localStorage.SetItem(SessionKey, serializedSession);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error while trying to save the session to the local storage: {ex}");
+        }
+    }
+
+    public void DestroySession() => _localStorage.RemoveItem(SessionKey);
+
+    public Session? LoadSession()
+    {
+        try
+        {
+            var serializedSession = _localStorage.GetItem<string>(SessionKey);
+
+            if (string.IsNullOrWhiteSpace(serializedSession))
+                return null;
+
+            var session = JsonConvert.DeserializeObject<Session>(serializedSession);
+
+            if (session == null || session.Expired())
+                return null;
+
+            return session;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error while trying to load the session from the local storage: {ex}");
+            return null;
+        }
+    }
+}
