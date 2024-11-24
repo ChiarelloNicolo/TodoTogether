913ac5618394c03146c9d977b67d79dcf4489721
Nicolo' Chiarello <chiarello.nicolo1@gmail.com>
2024-11-24 19:20:49 +0100

    added supabase client as injected service

--- src/TodoTogether/TodoTogether/Program.cs
+++ src/TodoTogether/TodoTogether/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using TodoTogether;
+using TodoTogether.Extensions;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.AddSupabase();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
